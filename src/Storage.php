9116d859ba0580729129bb664befb9258d58ed27
Ross Masters <ross@rossmasters.com>
2014-02-14 16:27:58 +0000

    Implement missing storage interface

--- src/Storage.php
+++ src/Storage.php
@@ -2,7 +2,13 @@
 
 namespace Feature;
 
+/**
+ * A storage provider
+ */
 interface Storage
 {
-
+   /**
+    * Load all of the storage's feature/toggles into the manager
+    */
+   public function load();
 }
